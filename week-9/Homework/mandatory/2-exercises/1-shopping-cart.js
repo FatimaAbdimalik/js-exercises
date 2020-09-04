e323f66c394022ea6bf5542a2e3a4e86a2e3c57b
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-09-04 21:18:28 +0100

    done

--- week-9/Homework/mandatory/2-exercises/1-shopping-cart.js
+++ week-9/Homework/mandatory/2-exercises/1-shopping-cart.js
@@ -12,12 +12,25 @@ The output of running your code should be:
 class ShoppingCart {
   // Add your code here
 
+  // constructor(_item1, _item2, _item3) {
+  constructor() {
+    // this.item1 = _item1;
+    // this.item2 = _item2;
+    // this.item3 = _item3;
+    this.ShoppingCart = [];
+  }
+
+  addItem(item) {
+    this.ShoppingCart.push(item);
+  }
   cartContains() {
-    // Use console.log() to output everything contained in your cart
+    // console.log(`My cart has ${this.item1}, ${this.item2} and ${this.item3}`);
+    // to output everything contained in your cart
+    console.log(`The shopping cart has ${this.ShoppingCart}`);
   }
 }
 
-let myCart = new ShoppingCart(); // Creates an empty shopping cart
+let myCart = new ShoppingCart("Toilet Roll", "Pasta", "Eggs"); // Creates an empty shopping cart
 
 myCart.addItem("Toilet Roll");
 myCart.addItem("Pasta");
